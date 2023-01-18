074e738a6bd92ef7118bee7298fb4191ea1518df
Peter Forret <peter@forret.com>
2023-01-18 15:02:28 +0100

    ADD: 2023-01-07-browser.md browser.jpg, MOD: _config.yml post.html abrp.jpg generate.sh gmaps.jpg hacker.jpg nuuz.jpg speed.jpg time.jpg weather.jpg index.html custom.css

--- images/generate.sh
+++ images/generate.sh
@@ -2,8 +2,9 @@
 
 splashmark -i "Google\nMaps" unsplash "google" gmaps.jpg
 splashmark -i "A Better\nRoute Planner" unsplash "route" abrp.jpg
-splashmark -i "Time" unsplash "clock" time.jpg
+splashmark -i "Exact\nTime" unsplash "clock" time.jpg
 splashmark -i "Weather\nGrimbergen" unsplash "weather" weather.jpg
 splashmark -i "Tesla\nNuuz.io" unsplash "tesla" nuuz.jpg
 splashmark -i "Hacker\nNews" unsplash "news" hacker.jpg
 splashmark -i "Network\nSpeed" unsplash "speed" speed.jpg
+splashmark -i "Browser\nDetective" unsplash "detective" browser.jpg
