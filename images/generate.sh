44541bd3db2d2f278d52a66f20e62faf23c93098
Peter Forret <peter@forret.com>
2023-01-19 10:21:50 +0100

    ADD: 2023-01-02-mixcloud.md 2023-01-02-vrtnu.md diw.jpg mixcloud.jpg diw.jpg mixcloud.jpg, MOD: _config.yml version.html abrp.jpg browser.jpg generate.sh gmaps.jpg hacker.jpg nuuz.jpg abrp.jpg browser.jpg gmaps.jpg hacker.jpg nuuz.jpg speed.jpg time.jpg weather.jpg speed.jpg time.jpg weather.jpg

--- images/generate.sh
+++ images/generate.sh
@@ -8,6 +8,8 @@ splashmark -i "Tesla\nNuuz.io" unsplash "tesla" nuuz.jpg
 splashmark -i "Hacker\nNews" unsplash "news" hacker.jpg
 splashmark -i "Network\nSpeed" unsplash "speed" speed.jpg
 splashmark -i "Browser\nDetective" unsplash "detective" browser.jpg
+splashmark -i "Mixcloud" unsplash "dj" mixcloud.jpg
+splashmark -i "De Ideale\nWereld" url "https://images.vrt.be/w1920hx_j80/2022/08/29/90ffcfe8-27d3-11ed-b07d-02b7b76bf47f.jpg" diw.jpg
 
 for image in *.jpg ; do
   convert -strip -interlace Plane -gaussian-blur 0.05 -quality 85% "$image" -resize 200x200 "small/$image"
